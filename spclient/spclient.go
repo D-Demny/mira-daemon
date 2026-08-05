5d132dca144ed45b92c9d07bc493d487583907cf
devgianlu <altomanigianluca@gmail.com>
2026-08-05 21:48:44 +0200

    fix: do not send requests with empty `Client-Token`
    (cherry picked from commit f1ef06fc2fb0d34ac3baede726b37de99953c237)

--- spclient/spclient.go
+++ spclient/spclient.go
@@ -8,6 +8,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"maps"
 	"net/http"
 	"net/url"
 	"strconv"
@@ -67,12 +68,12 @@ func (c *Spclient) innerRequest(ctx context.Context, method string, reqUrl *url.
 	}
 
 	if header != nil {
-		for name, values := range header {
-			req.Header[name] = values
-		}
+		maps.Copy(req.Header, header)
 	}
 
-	req.Header.Set("Client-Token", c.clientToken)
+	if len(c.clientToken) > 0 {
+		req.Header.Set("Client-Token", c.clientToken)
+	}
 
 	if body != nil {
 		// default protobuf, caller can pin json (used by connect-state commands)
